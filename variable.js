2280a8a538095a0f42098805579c9a68586e9986
maicol <maykolandres4@gmail.com>
2022-12-05 05:26:22 -0500

    learning about variables

---
+++ variable.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var greetings = "hello maicol";
+greetings.toLowerCase();
+console.log(greetings);
+//number 
+var userId = 34314;
+//boolean
+var isLoggedIn = false;
---
+++ variable.ts
@@ -0,0 +1,28 @@
+let greetings: string = "hello maicol";
+
+
+greetings.toLowerCase()
+
+console.log(greetings)
+
+
+//number 
+
+let userId = 34314;
+
+//boolean
+
+let isLoggedIn: boolean = false
+
+
+//any
+
+let hero: string;
+
+function getHero(){
+    return "thor"
+}
+
+hero = getHero()
+
+export{}
