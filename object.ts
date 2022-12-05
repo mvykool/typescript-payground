782f7d27e01c33a10942d893c6af18ce8c665f93
maicol <maykolandres4@gmail.com>
2022-12-05 06:24:37 -0500

    creating objects

---
+++ object.ts
@@ -0,0 +1,19 @@
+const User = {
+    name: "maicol",
+    email: "maicol@hl.com",
+    isActive: false
+}
+
+
+function createUser({name: string, isPaid: boolean}){
+
+}
+
+createUser({name: "maicol", isPaid: true})
+
+
+function createCourse():{name: string, isPaid: boolean}{
+    return {name: "react", isPaid: false}
+}
+
+export{}
