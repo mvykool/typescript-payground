74ac6c98c15cbccdb50353d90a678858ce8b4fc7
maicol <maykolandres4@gmail.com>
2022-12-05 08:10:32 -0500

    mixing types

--- object.ts
+++ object.ts
@@ -30,5 +30,40 @@ function createUserNow (user: User): User {
     return({name: "", email: "", isActive: true})
 }
 
+//readonly when you do not want a element to change
+
+type UserReadonly = {
+   readonly _id: string
+   name: string
+   email: string
+   isActive: boolean
+
+   //wjem we put a ?  means that its optional
+   cradircard?: number
+}
+
+let myUser: UserReadonly = {
+    _id: "nddisau",
+    name: "h",
+    email: "h@h.com",
+    isActive: false
+
+}
+
+
+//mix types not a very good practice but is doable
+
+type cardNumber = {
+    cardnumber: string
+}
+
+type cardDate = {
+    date: string
+}
+
+type cardDetails = cardDate & cardNumber & {
+    cvv: string
+}
+
 
 export{}
